2c7020e20e3b548d8b5930777b5e61a7564cef88
thevisualriot <rydzkowska@gmail.com>
2023-12-24 16:00:23 +0000

    fix error handling

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* *********************************************************    VARIABLES   *********************************************************** */
+/* *********************************************************    GLOBAL VARIABLES   *********************************************************** */
 
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
@@ -12,8 +12,6 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-let employeeType;
-
 let manager = "";
 let employee = "";
 let team = [];
@@ -21,6 +19,7 @@ let team = [];
 
 /* *********************************************************    QUESTIONS   *********************************************************** */
 const questions = [
+    // MANAGER
     {
         type: 'input',
         name: 'manager name',
@@ -84,14 +83,14 @@ const questions = [
     },
 ];
 
+// Second run of questions when additional employee added
 const additionalQuestions = [4, 5, 6, 7, 8, 9];
 const secondaryQuestionaire = additionalQuestions.map((index) => questions[index]);
 
 
-
 /* ***********************************************************   FUNCTIONS   *************************************************************** */
 
-
+// RUN QUESTIONAIRE 
 function runQuestionaire(questions, index) {
     inquirer.prompt(questions.slice(index))
         .then((answers) => {
@@ -100,33 +99,36 @@ function runQuestionaire(questions, index) {
                 return;
             }
 
-            if(!manager){
+            if(!manager){ // If manager was not added, add it to the array of team, otherwise skip
                 manager = new Manager(answers['manager name'], answers['manager ID'], answers['manager email'], answers['office number'])
                 manager.role = "manager";
+                console.log("here 1: " + typeof manager.id);
                 addMember(manager);
             }
             
 
-            if(answers.employee === "Add an engineer"){
+            if(answers.employee === "Add an engineer"){ // add engineer
                 employee = new Engineer(answers['employee name'], answers['employee id'], answers['employee email'], answers['github']);
+                console.log("here: " + typeof employee.id);
                 employee.role = "engineer";
                 addMember(employee);
-            } else if (answers.employee === "Add an intern"){
+            } else if (answers.employee === "Add an intern"){ // add intern
                 employee = new Intern(answers['employee name'], answers['employee id'], answers['employee email'], answers['school']);
                 employee.role = "intern";
                 addMember(employee);
             }
 
-            runQuestionaire(questions, 4);
+            runQuestionaire(questions, 4); // when adding new employee run questions from the 5th, to skip manager questions
         })
         .catch((err) => console.log(err));
 }
 
-
+// ADD MEMBER TO 'TEAM' ARRAY
 function addMember(val) {
     team.push(val);
 }
 
+// RENDER TO HTML FILE
 function renderData() {
     data = render(team);
     fs.writeFile(outputPath, data, (err) => {
@@ -135,22 +137,7 @@ function renderData() {
 }
 
 
+/* **************************************************************   LET'S GO!   ********************************************************** */
 
-/* **************************************************************   LET'S GO BABY!   ********************************************************** */
-
+// Run the app
 runQuestionaire(questions, 0)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
